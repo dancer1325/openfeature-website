2aa98d80a36d46119236018ea7d02d832efaa8d6
alfredo-toledano <alfredo.toledano@allianzdirect.es>
2024-06-01 11:45:56 +0200

    doc(technologies.server.java): add notes about targetting

--- docs/reference/technologies/server/java/tutorial/src/main/java/com/example/tutorial/HelloServlet.java
+++ docs/reference/technologies/server/java/tutorial/src/main/java/com/example/tutorial/HelloServlet.java
@@ -11,7 +11,10 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import dev.openfeature.sdk.Client;
+import dev.openfeature.sdk.EvaluationContext;
+import dev.openfeature.sdk.ImmutableContext;
 import dev.openfeature.sdk.OpenFeatureAPI;
+import dev.openfeature.sdk.Value;
 import dev.openfeature.sdk.providers.memory.Flag;
 import dev.openfeature.sdk.providers.memory.InMemoryProvider;
 
@@ -28,17 +31,38 @@ public class HelloServlet extends HttpServlet {
                 .defaultVariant("on")
                 .build());
 
-        // configure a provider
+        // set EvaluationContext
+        Map<String, Value> apiAttrs = new HashMap<>();
+        apiAttrs.put("region", new Value(System.getenv("us-east-1")));
+        EvaluationContext apiCtx = new ImmutableContext(apiAttrs);
+
+
         OpenFeatureAPI api = OpenFeatureAPI.getInstance();
+        // 1. configure a provider
+        // 1.1 syn
         api.setProviderAndWait(new InMemoryProvider(myFlags));
-        // async way to register the provider
+        // 1.2 async
 //        api.setProvider(new InMemoryProvider(myFlags));
+        // 2. pas the EvaluationContext
+        api.setEvaluationContext(apiCtx);
 
         // create a client
         Client client = api.getClient();
+        // 1. set EvaluationContext
+        client.setEvaluationContext(apiCtx);
 
+        // set a value to the invocation context
+        // TODO: How to get session?
+        /*Map<String, Value> requestAttrs = new HashMap<>();
+        requestAttrs.put("email", new Value(Session.getAttribute("email")));
+        requestAttrs.put("product", new Value("productId"));
+        String targetingKey = session.getId();
+        EvaluationContext reqCtx = new ImmutableContext(targetingKey, requestAttrs);
+*/
         // get a bool flag value
         boolean flagValue = client.getBooleanValue("v2_enabled", false);
+        // TODO: Once I get where session comes from
+        //boolean flagValue = client.getBooleanValue("v2_enabled", false, reqCtx);
         message = flagValue ? "Hello World! - v2_enabled" : "Bye World! - v2_enabled";
     }
 
